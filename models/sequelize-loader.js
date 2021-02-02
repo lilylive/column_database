2e1c52f005300287af92a218f2f891d4aa63e67b
hiroyuki_maruko <rakugodaisuki2019@gmail.com>
2021-02-02 16:31:16 +0000

    ブランチ切り替え、host修正Heroku対応

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -2,7 +2,7 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/schedule_arranger',
+  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/column_database',
   {
     operatorsAliases: false
   });
