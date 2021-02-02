82906417bbd2b68c76aa1689359b015ddfd096bd
hiroyuki_maruko <rakugodaisuki2019@gmail.com>
2021-02-02 17:51:08 +0000

    Heroku対応

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,8 @@
 'use strict';
 import $ from 'jquery';
+const global = Function('return this;')();
+global.jQuery = $;
+import bootstrap from 'bootstrap';
 const uuid = require('uuid');
 
 $('.admit-button').each((i, e) => {
