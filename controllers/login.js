313f80f64858451258ccd43b41716a3a397d972c
hiroyuki_maruko <rakugodaisuki2019@gmail.com>
2021-03-24 02:27:03 +0000

    オープンリダイレクト対応

--- controllers/login.js
+++ controllers/login.js
@@ -3,7 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 function doLogin (req, res, next)  {
-  res.render('login', { user: req.user });
+    const from = req.query.from;
+  if (from) {
+    res.cookie('loginFrom', from, { expires: new Date(Date.now() + 600000)});
+  }
+  res.render('login');
+  //res.render('login', { user: req.user });
 };
 
 module.exports = doLogin;
--- routes/authentication-ensurer.js
+++ routes/authentication-ensurer.js
@@ -2,7 +2,8 @@
 
 function ensure(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login');
+  res.redirect('/login?from=' + req.originalUrl);
+  //res.redirect('/login');
 }
 
 module.exports = ensure;
